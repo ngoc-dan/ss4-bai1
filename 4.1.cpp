4fc66404b01ddae45892f4b452a6ce0af5c672f3
ngoc-dan <ngocdan191026@gmail.com>
2024-11-24 20:15:58 +0700

    Add files via upload

---
+++ 4.1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int n;
+	printf("nhap so n = ");
+	scanf("%d" , n);
+	if(n<0){
+		printf("n la so am %d\n" , n);
+	}
+	else{
+	    printf("n la so duong", n);
+	}
+	
+	return 0;
+}
